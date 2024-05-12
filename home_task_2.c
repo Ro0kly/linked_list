00d7b892d85abc0af13c636a60d37318959711ab
Ro0kly <snallytango@yandex.ru>
2024-05-12 19:45:07 +0300

    home_task / add comments

--- home_task_2.c
+++ home_task_2.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+Двунаправленный кольцевой список (вариант данных 3).
+Упорядочить список в порядке возрастания номеров счетов.
+Подсчитать количество вкладчиков и общую сумму их вкладов для вкладов,
+превышающих заданную величину.
+*/
+
+// Comment: Данная структура Person является нодой двунаправленного списка.
 typedef struct person {
 	int id;
 	char *second_name;
@@ -12,6 +20,10 @@ typedef struct person {
 	struct person *next;
 } Person;
 
+/*
+Comment: Данная функция разбивает строку на массив слов (матрица).
+Возвращает количество полученных слов.
+*/
 int separate_words(char *text, char **words) {
 	char *ptr = strtok(text, " ");
 	int j = 0;
@@ -25,6 +37,7 @@ int separate_words(char *text, char **words) {
 	return j;
 }
 
+// Comment: Данная функция создает ноду Person и возвращает ее.
 Person *create_person(char **person_details, int details_count) {
 	if (details_count < 5) {
 		return NULL;
@@ -40,6 +53,7 @@ Person *create_person(char **person_details, int details_count) {
 	return person;
 }
 
+// Comment: Данная функция пушит ноду в список (делает хвостовой).
 void push(Person *head, Person *next) {
 	if (head == NULL) {
 		printf("Head is NULL");
@@ -53,11 +67,16 @@ void push(Person *head, Person *next) {
 	next->prev = current;
 }
 
+// Comment: Данная функция печатает одну ноду.
 void print_person(Person *person) {
 	printf("person: %d, %s, %s, %d, %d\n", person->id, person->second_name, person->short_name, person->account_number,
 	       person->deposit);
 }
 
+/*
+Comment: Данная функция печатает список. 
+Поскольку список кольцевой, ожидаем когда current дойдет до head.
+*/
 void print_persons(Person *head) {
 	Person *current = head;
 	do {
@@ -69,6 +88,10 @@ void print_persons(Person *head) {
 	} while (1);
 }
 
+/*
+Comment: Данная функция читает файл persons.txt по строкам
+и создает из вкладчиков (данные строк) ноду, а затем пушит в лист.
+*/
 void create_list_from_file(FILE *fptr, Person **head, Person **tail, int *count) {
 	const int str_length = 1000;
 	char str[str_length];
@@ -95,6 +118,7 @@ void create_list_from_file(FILE *fptr, Person **head, Person **tail, int *count)
 	}
 }
 
+//Comment: Данная функция меняет местами данные вкладчиков.
 void swap_info(Person **current) {
 	int tmp_id = (*current)->id;
 	char *tmp_second_name = (*current)->second_name;
@@ -115,6 +139,7 @@ void swap_info(Person **current) {
 	(*current)->next->deposit = tmp_deposit;
 }
 
+//Данная функция сортирует вкладчиков (ноды) по номеру счета (account_number).
 void sort_by_account_number(Person **head, Person **tail) {
 	int swap = 0;
 	(*head)->prev = NULL;
@@ -142,23 +167,31 @@ void sort_by_account_number(Person **head, Person **tail) {
 	printf("\n");
 }
 
+/*
+Comment: Данная функция принимает опорное значение
+и считает сумму депозитов выше этого значения.
+*/
 void get_sum_by_pivot(Person *head, int pivot) {
-    int sum = 0;
-    Person *current = head;
-    do {
-        if (current->deposit > pivot) {
-            sum += current->deposit;
-        }
-        current = current->next;
-    } while (current != head);
-    printf("Опорная сумма: %d, сумма депозитов счетов выше опорный суммы: %d\n", pivot, sum);
+	int sum = 0;
+	Person *current = head;
+	do {
+		if (current->deposit > pivot) {
+			sum += current->deposit;
+		}
+		current = current->next;
+	} while (current != head);
+	printf("Опорная сумма: %d, сумма депозитов счетов выше опорный суммы: %d\n", pivot, sum);
 }
 
+/*
+Comment: Это функция main, здесь программа начинает свое выполнение,
+открываем файл для чтения.
+*/
 int main() {
 	Person *head = NULL;
 	Person *tail = NULL;
 	int person_count = 0;
-    int pivot = 10000;
+	int pivot = 10000;
 
 	FILE *fptr = NULL;
 	fptr = fopen("persons.txt", "r");
@@ -167,17 +200,17 @@ int main() {
 	} else {
 		printf("Файл готов к чтению.\n");
 		create_list_from_file(fptr, &head, &tail, &person_count);
-        printf("--------------------------\n");
-        printf("Начальный список вкладчиков (до изменений):\n");
+		printf("--------------------------\n");
+		printf("Начальный список вкладчиков (до изменений):\n");
 		print_persons(head);
-        printf("--------------------------\n");
+		printf("--------------------------\n");
 		printf("Количество вкладчиков: %d\n\n", person_count);
-        printf("Список вкладчиков остортированный по возрастанию номеров счетов:\n");
+		printf("Список вкладчиков остортированный по возрастанию номеров счетов:\n");
 		sort_by_account_number(&head, &tail);
 		print_persons(head);
-        printf("\n");
-        get_sum_by_pivot(head, pivot);
-        printf("--------------------------\n");
+		printf("\n");
+		get_sum_by_pivot(head, pivot);
+		printf("--------------------------\n");
 		fclose(fptr);
 	}
 	return 0;
